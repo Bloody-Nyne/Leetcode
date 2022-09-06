2c8ecb690f39cd1267362db45688cfe7b221b4a0
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2022-09-06 16:54:58 +0530

    Time: 21 ms (23.14%), Space: 14.3 MB (25.28%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -16,12 +16,48 @@ private:
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int iniColor=image[sr][sc];
+      /*  int iniColor=image[sr][sc];
         vector<vector<int>> ans=image;
         int delrow[]={-1,0,1,0};
         int delcol[]={0,1,0,-1};
         dfs(sr,sc,ans,image,color,delrow,delcol,iniColor);
         
+        return ans; */
+        
+        //using BFS
+        int iniColor=image[sr][sc];
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<int>> ans=image;
+        queue<pair<int,int>> q;
+        ans[sr][sc] = color;
+        q.push({sr,sc});
+        
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
+                image[nrow][ncol]==iniColor && ans[nrow][ncol] != color){
+                    q.push({nrow,ncol});
+                    ans[nrow][ncol] = color;
+                }
+            }
+        }
         return ans;
     }
+    
+    
+    
+    
+    
+    
+    
 };
