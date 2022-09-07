3900d3b2927de526c73578c246dbdc075e894a3a
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2022-09-07 14:53:30 +0530

    Time: 28 ms (5.68%), Space: 14.3 MB (5.70%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+private:
+    int getFirst(vector<int> nums,int n,int target){
+        int s=0,e=n-1,mid;
+        int ans=-1;
+        while(s <= e){
+            mid=s+(e-s)/2;
+            if(nums[mid] == target){
+                if(mid == s || nums[mid] != nums[mid-1]){
+                    ans=mid;
+                    break;
+                }
+                else{
+                    ans=mid-1;
+                    e=mid-1;
+                }
+            }
+            else if(nums[mid] < target) s=mid+1;
+            else
+                e=mid-1;
+        }
+        return ans;
+    }
+    
+    int getLast(vector<int> nums,int n,int target){
+        int s=0,e=n-1,mid;
+        int ans=-1;
+        while(s <= e){
+            mid=s+(e-s)/2;
+            if(nums[mid] == target){
+                if(mid == e || nums[mid] != nums[mid+1]){
+                    ans=mid;
+                    break;
+                }
+                else{
+                    ans=mid+1;
+                    s=mid+1;
+                }
+            }
+            else if(nums[mid] < target) s=mid+1;
+            else
+                e=mid-1;
+        }
+        return ans;
+    }
+    
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> ans;
+        int first=getFirst(nums,n,target);
+        int last=getLast(nums,n,target);
+        ans.push_back(first);
+        ans.push_back(last);
+        
+        return ans;
+    }
+};
