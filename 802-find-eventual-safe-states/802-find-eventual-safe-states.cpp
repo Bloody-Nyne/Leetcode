4b5b43bc87465176dc4576b101084b21409cd70d
Harshvardhan singh <102972384+Bloody-Nyne@users.noreply.github.com>
2022-09-11 17:11:11 +0530

    Time: 439 ms (13.15%), Space: 61.7 MB (15.07%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -25,7 +25,8 @@ private:
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> safeNodes;
+        //USING DFS ->
+     /*   vector<int> safeNodes;
         int n=graph.size();
         vector<int> vis(n,0);
         vector<int> path(n,0);
@@ -39,6 +40,38 @@ public:
         for(int i=0;i<n;i++){
             if(checkNodes[i] == 1) safeNodes.push_back(i);
         }
-        return safeNodes;
+        return safeNodes; */
+        
+        //USING BFS(Kahn's Algo) ->
+        
+        int n=graph.size();
+        int i=0;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+        for(auto &g : graph){
+            for(auto &it: g){
+                adj[it].push_back(i);
+                indegree[i]++;
+            }
+            i++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;    
+        
     }
 };
