e7e51f6749167e4abdafabe2897436587d1a6105
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-09-13 22:36:50 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (49.25%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,26 +12,26 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> maps;
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        
+        map<int,map<int,multiset<int>>> mpp;
         queue<pair<TreeNode*,pair<int,int>>> q;
         q.push({root,{0,0}});
         while(!q.empty()){
-            auto node=q.front();
+            auto temp=q.front();
             q.pop();
-            TreeNode* frontNode=node.first;
-            int dist=node.second.first;
-            int level=node.second.second;
-            maps[dist][level].insert(frontNode->val);
             
-            if(frontNode->left){
-                q.push({frontNode->left,{dist-1,level+1}});
-            }
-            if(frontNode->right){
-                q.push({frontNode->right,{dist+1,level+1}});
-            }
+            TreeNode* node=temp.first;
+            int hd=temp.second.first;
+            int level=temp.second.second;
+            
+            mpp[hd][level].insert(node->val);
+            if(node->left) q.push({node->left,{hd-1,level+1}});
+            if(node->right) q.push({node->right,{hd+1,level+1}});
         }
-        vector<vector<int>> ans;
-        for(auto i: maps){
+
+        for(auto i: mpp){
             vector<int> temp;
             for(auto j: i.second){
                 temp.insert(temp.end(),j.second.begin(),j.second.end());
