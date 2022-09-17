cc257de25af5b926e0da9ede889600eacb3c2e7a
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-09-17 21:17:55 +0530

    Time: 9 ms (24.10%), Space: 7.5 MB (98.65%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -15,12 +15,25 @@ private:
             }
         }
     }
+    void BackTrack(vector<int>& nums,int ind,vector<vector<int>>& ans){
+        if(ind == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            BackTrack(nums,ind+1,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> temp;
+       /* vector<int> temp;
         vector<int> map(nums.size(),0);
-        backTrack(nums,temp,map,ans);
+        backTrack(nums,temp,map,ans); */
+        
+        BackTrack(nums,0,ans);
         return ans;
     }
 };
