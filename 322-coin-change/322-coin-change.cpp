9daa663a678e438028912c417cedcccc27532b14
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-10-01 01:22:53 +0530

    Time: 165 ms (45.64%), Space: 45.3 MB (26.68%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Memoization
     int count(int ind,int target,vector<int>& nums,vector<vector<int>>& dp){
         if(ind == 0) {
             if(target%nums[0] == 0) return target/nums[0];
@@ -15,8 +16,22 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans = count(n-1,amount,coins,dp);
+        //tabulation
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int t=0;t<=amount;t++) {
+            if(t % coins[0] == 0) dp[0][t] = t/coins[0];
+            else dp[0][t] = 1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                int notTake = 0 + dp[i-1][t];
+                int take = 1e9;
+                if(coins[i] <= t) 
+                    take = 1 + dp[i][t-coins[i]];
+                dp[i][t] = min(take,notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans >= 1e9) return -1;
         return ans;
     }
