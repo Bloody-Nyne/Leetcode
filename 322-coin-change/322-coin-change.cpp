7ea5641475d4bdfcf17400e51dcd6f24a4b03eb7
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-10-01 01:28:13 +0530

    Time: 238 ms (20.35%), Space: 18.7 MB (34.74%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -17,21 +17,22 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         //tabulation
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0),cur(amount+1,0);
         for(int t=0;t<=amount;t++) {
-            if(t % coins[0] == 0) dp[0][t] = t/coins[0];
-            else dp[0][t] = 1e9;
+            if(t % coins[0] == 0) prev[t] = t/coins[0];
+            else prev[t] = 1e9;
         }
         for(int i=1;i<n;i++){
             for(int t=0;t<=amount;t++){
-                int notTake = 0 + dp[i-1][t];
+                int notTake = 0 + prev[t];
                 int take = 1e9;
                 if(coins[i] <= t) 
-                    take = 1 + dp[i][t-coins[i]];
-                dp[i][t] = min(take,notTake);
+                    take = 1 + cur[t-coins[i]];
+                cur[t] = min(take,notTake);
             }
+            prev = cur;
         }
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         if(ans >= 1e9) return -1;
         return ans;
     }
