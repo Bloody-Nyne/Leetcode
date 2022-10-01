1efc473266932c5381b723f96a4d10d5c9b748e7
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-10-02 00:33:39 +0530

    Time: 32 ms (59.20%), Space: 7.3 MB (81.38%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int solve(int ind,int target,vector<int>& nums,vector<vector<int>>& dp){
+        //memoization
         if(ind == 0){
             if(target%nums[0] == 0) return 1;
             return 0;
@@ -13,8 +14,9 @@ public:
         
         return dp[ind][target] = take + notTake;
     }
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
+    
+    int tabulate(int n,int amount,vector<int>& coins){
+        //tabulation
         vector<vector<int>> dp(n,vector<int>(amount+1,0));
         for(int t=0;t<=amount;t++){
             if(t%coins[0] == 0) dp[0][t] = 1;
@@ -30,6 +32,28 @@ public:
         }
         int ans = 0;
         ans = dp[n-1][amount];
+        return ans;
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        //tabulation with space optimization
+        vector<int> prev(amount+1,0),cur(amount+1,0);
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0] == 0) prev[t] = 1;
+            else prev[t] = 0;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int t=0;t<=amount;t++){
+                int notTake = prev[t];
+                int take = 0;
+                if(coins[ind] <= t) take = cur[t-coins[ind]];
+                cur[t] = take + notTake;
+            }
+            prev = cur;
+        }
+        int ans = 0;
+        ans = prev[amount];
         return ans;
     }
 };
