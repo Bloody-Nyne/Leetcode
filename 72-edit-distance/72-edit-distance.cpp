46406a22fb3825bfce869be42446672108b150b6
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-10-05 19:58:22 +0530

    Time: 20 ms (61.28%), Space: 6.5 MB (93.93%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int solve(int i,int j,string &s,string &t,vector<vector<int>>& dp){
+        //Memoization
         if(i == 0) return j;
         if(j == 0) return i;
         if(dp[i][j] != -1) return dp[i][j];
@@ -14,20 +15,23 @@ public:
         }
     }
     int minDistance(string s, string t) {
+        //Tabulation with space optimization
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int j=0;j<=m;j++) dp[0][j] = j;
-        for(int i=0;i<=n;i++) dp[i][0] = i;
+        vector<int> prev(m+1,0), cur(m+1,0);
+        for(int j=0;j<=m;j++) prev[j] = j;
+     //   for(int i=0;i<=n;i++) dp[i][0] = i;
         for(int i=1;i<=n;i++){
+            cur[0] = i;
             for(int j=1;j<=m;j++){
                 if(s[i-1] == t[j-1])
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
                 else{
-                    dp[i][j] = min( 1 + dp[i][j-1],min(1 + dp[i-1][j],1 + dp[i-1][j-1]));
+                    cur[j] = min( 1 + cur[j-1],min(1 + prev[j],1 + prev[j-1]));
                 }
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
