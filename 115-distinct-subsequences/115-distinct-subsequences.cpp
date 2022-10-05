d7313437ab30140c6fd43da257eb65a497b3e514
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-10-05 17:50:53 +0530

    Time: 28 ms (92.16%), Space: 6.4 MB (89.47%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
     int solve(int i,int j,string &s,string &t,vector<vector<int>>& dp){
+        //memeoization
         if(j < 0) return 1;
         if(i < 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
@@ -12,18 +13,20 @@ private:
     }    
 public:
     int numDistinct(string s, string t) {
+        //tabulation with space optimization
         int n = s.size();
         int m = t.size();
-        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0] = 1;
+        vector<double> prev(m+1,0), cur(m+1,0);
+        prev[0] = cur[0] = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1] == t[j-1])
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    cur[j] = prev[j-1] + prev[j];
                 else
-                    dp[i][j] = dp[i-1][j];
+                    cur[j] = prev[j];
             }
+            prev = cur;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
