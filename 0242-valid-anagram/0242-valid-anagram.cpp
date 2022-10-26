cd911dfb2d46f86430619c9d6836a6ee58779f13
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-10-26 09:03:39 +0530

    Time: 29 ms (25.55%), Space: 7.3 MB (56.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<t.length() || i<s.length();i++){
+            if(s[i] != t[i]) return false;
+        }
+        return true;
+    }
+};
