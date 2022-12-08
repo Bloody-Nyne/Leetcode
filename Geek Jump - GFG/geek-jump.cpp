cab06ac738264fe7b0864cd203f6211892cc509c
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-12-08 19:47:42 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -7,8 +7,18 @@ using namespace std;
 class Solution {
   public:
     int minimumEnergy(vector<int>& height, int n) {
-        vector<int> dp(n,-1);
-        return solve(n-1,height,dp);
+        vector<int> dp(n,0);
+       // return solve(n-1,height,dp);
+        dp[0] = 0;
+        for(int ind=1;ind<n;ind++){
+            int jumpOne = abs(height[ind] - height[ind-1]) + dp[ind-1];
+            int jumpTwo = 1e9;
+            if(ind > 1)
+                jumpTwo = abs(height[ind] - height[ind-2]) + dp[ind-2];
+            
+            dp[ind] = min(jumpOne,jumpTwo);
+        }
+        return dp[n-1];
     }
 private:
     int solve(int ind,vector<int>& height,vector<int>& dp){
