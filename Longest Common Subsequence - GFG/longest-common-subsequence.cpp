32838eb10ae9550e7eb7f64465a67773dadb4ba5
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2022-12-22 21:24:04 +0530

    Added solution - LeetHub

---
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        vector<vector<int>> dp(x,vector<int>(y,-1));
+        return slave(x-1,y-1,s1,s2,dp);
+    }
+    private:
+    int slave(int ind1,int ind2,string s,string t,vector<vector<int>>& dp){
+        if(ind1 < 0 || ind2 < 0) return 0;
+        
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        
+        if(s[ind1] == t[ind2]) return dp[ind1][ind2] = 1 + slave(ind1-1,ind2-1,s,t,dp);
+        else return dp[ind1][ind2] = 0 + max(slave(ind1-1,ind2,s,t,dp), slave(ind1,ind2-1,s,t,dp));
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t,n,k,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(x, y, s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
