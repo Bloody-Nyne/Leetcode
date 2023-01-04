db54625c33e9c269940d5016b87fa7abf79165b7
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-01-04 14:10:32 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -8,8 +8,19 @@ class Solution {
     int editDistance(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return slave(n-1,m-1,s,t,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        //return slave(n-1,m-1,s,t,dp);
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+        for(int j=0;j<=m;j++) dp[0][j] = j;
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s[ind1-1] == t[ind2-1]) 
+                    dp[ind1][ind2] = 0 + dp[ind1-1][ind2-1];
+                else
+                    dp[ind1][ind2] =  1 + min({dp[ind1][ind2-1], dp[ind1-1][ind2], dp[ind1-1][ind2-1]});
+            }
+        }
+        return dp[n][m];
     }
   private:
     int slave(int ind1,int ind2,string s,string t,vector<vector<int>>& dp){
