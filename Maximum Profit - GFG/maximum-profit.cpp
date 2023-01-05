29a4aa6c7cf5cbb06c316e65718b601b8a5c5146
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-01-05 22:59:07 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -10,8 +10,27 @@ using namespace std;
 class Solution {
   public:
     int maxProfit(int K, int N, int A[]) {
-        vector<vector<vector<int>>> dp(N,vector<vector<int>>(2,vector<int>(K+1,-1)));
-        return slave(0,true,K,A,N,dp);
+        vector<vector<vector<int>>> dp(N+1,vector<vector<int>>(2,vector<int>(K+1,0)));
+        //return slave(0,true,K,A,N,dp);
+        dp[N][0][0] = dp[N][1][0] = 0;
+        for(int ind=N-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int limit=1;limit<=K;limit++){
+                    int notTake = -1e9;
+                    int take = -1e9;
+                    if(buy){
+                        notTake = 0 + dp[ind+1][true][limit];
+                        take = -A[ind] + dp[ind+1][false][limit];
+                    }
+                    else{
+                        notTake = 0 + dp[ind+1][false][limit];
+                        take = A[ind] + dp[ind+1][true][limit-1];
+                    }
+                    dp[ind][buy][limit] = max(notTake,take);
+                }
+            }
+        }
+        return dp[0][1][K];
     }
 private:
     int slave(int ind,int buy,int limit,int prices[],int N,vector<vector<vector<int>>>& dp){
