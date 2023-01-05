ddcd0c0cb7459b83d34df4155e2b39d0e753f62a
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-01-05 15:56:15 +0530

    Added solution - LeetHub

--- Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
@@ -27,8 +27,27 @@ int slave(int ind,int buy,int limit,vector<int>& price,vector<vector<vector<int>
 
 int maxProfit(vector<int>&price){
     int n = price.size();
-    vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-    return slave(0,true,2,price,dp);
+    vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+    //return slave(0,true,2,price,dp);
+    dp[n][0][0] = dp[n][1][0] = 0;
+    for(int ind=n-1;ind>=0;ind--){
+        for(int buy=0;buy<2;buy++){
+            for(int limit=1;limit<3;limit++){
+                int take = -1e9;
+                int notTake = -1e9;
+                if(buy){
+                    notTake = 0 + dp[ind+1][true][limit];
+                    take = -price[ind] + dp[ind+1][false][limit];
+                }
+                else{
+                    notTake = 0 + dp[ind+1][false][limit];
+                    take = price[ind] + dp[ind+1][true][limit-1];
+                }
+                dp[ind][buy][limit] = max(notTake,take);
+            }
+        }
+    }
+    return dp[0][1][2];
 }
 
 //{ Driver Code Starts.
