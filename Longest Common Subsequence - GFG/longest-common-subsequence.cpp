d467b6041920caec98928acdf1be532a3a0ea08a
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-08-10 15:01:08 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,28 +10,34 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int lcs(int x, int y, string s1, string s2)
+    int solve(int i,int j,string s,string t,vector<vector<int>>& dp){
+        if(i <= 0 || j <= 0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s[i-1] == t[j-1]){
+            return dp[i][j] = 1 + solve(i-1,j-1,s,t,dp);
+        }
+        else{
+            return dp[i][j] = 0 + max(solve(i-1,j,s,t,dp), solve(i,j-1,s,t,dp));
+        }
+    }
+    int lcs(int n, int m, string s, string t)
     {
-        vector<vector<int>> dp(x+1,vector<int>(y+1,0));
-        //return slave(x-1,y-1,s1,s2,dp);
-        for(int i=0;i<=x;i++) dp[i][0] = 0;
-        for(int j=0;j<=y;j++) dp[0][j] = 0;
-        for(int i=1;i<=x;i++){
-            for(int j=1;j<=y;j++){
-                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        //return solve(n,m,s1,s2,dp);
+        dp[0][0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        return dp[x][y];
-    }
-    private:
-    int slave(int ind1,int ind2,string s,string t,vector<vector<int>>& dp){
-        if(ind1 < 0 || ind2 < 0) return 0;
-        
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        
-        if(s[ind1] == t[ind2]) return dp[ind1][ind2] = 1 + slave(ind1-1,ind2-1,s,t,dp);
-        else return dp[ind1][ind2] = 0 + max(slave(ind1-1,ind2,s,t,dp), slave(ind1,ind2-1,s,t,dp));
+        return dp[n][m];
     }
 };
 
@@ -39,15 +45,15 @@ class Solution
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
