f78819515c5de2af81309d42bf3156167c19969b
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-08-22 20:19:53 +0530

    Time: 536 ms (36.51%), Space: 292.2 MB (16.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int solve(int ind,int prev,vector<int>& nums,vector<vector<int>>& dp){
-        if(ind == nums.size()) return 0;
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
-        
-        int notTake = 0 + solve(ind+1,prev,nums,dp);
-        int take = -1e9;
-        if(prev == -1 || nums[ind] > nums[prev]) 
-            take = 1 + solve(ind+1,ind,nums,dp);
-        
-        return dp[ind][prev+1] = max(take,notTake);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> next(n+1,0),cur(n+1,0);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        //return slave(0,-1,nums,dp);
         for(int ind=n-1;ind>=0;ind--){
-            for(int prev=ind-1;prev>=-1;prev--){
-                int notTake = 0 + next[prev+1];
-                int take = -1e9;
-                if(prev == -1 || nums[ind] > nums[prev]) 
-                    take = 1 + next[ind+1];
-
-                cur[prev+1] = max(take,notTake);
+            for(int p_ind=ind-1;p_ind>=-1;p_ind--){
+                int notTake = 0 + dp[ind+1][p_ind+1];
+                int take = 0;
+                if(p_ind == -1 || nums[ind] > nums[p_ind]){
+                    take = 1 + dp[ind+1][ind+1];
+                }
+                dp[ind][p_ind+1] = max(take,notTake);
             }
-            next = cur;
         }
-        return next[-1+1];
+        return dp[0][0];
+    }
+private:
+    int slave(int ind,int p_ind,vector<int>& nums,vector<vector<int>>& dp){
+        if(ind == nums.size())
+            return 0;
+
+        if(dp[ind][p_ind+1] != -1) 
+            return dp[ind][p_ind+1];
+
+        int notTake = 0 + slave(ind+1,p_ind,nums,dp);
+        int take = 0;
+        if(p_ind == -1 || nums[ind] > nums[p_ind]){
+            take = 1 + slave(ind+1,ind,nums,dp);
+        }
+        return dp[ind][p_ind+1] = max(notTake,take);
     }
 };
