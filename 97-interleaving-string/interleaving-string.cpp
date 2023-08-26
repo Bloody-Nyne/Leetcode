95430cb2eaee0f90a289efe6523500538ee18c5a
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-08-26 14:41:20 +0530

    Time: 3 ms (78.55%) | Memory: 18.2 MB (34.78%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-     bool isInterleave(string s1, string s2, string s3) {
-    
-    if(s3.length() != s1.length() + s2.length())
-        return false;
-    
-    bool table[s1.length()+1][s2.length()+1];
-    
-    for(int i=0; i<s1.length()+1; i++)
-        for(int j=0; j< s2.length()+1; j++){
-            if(i==0 && j==0)
-                table[i][j] = true;
-            else if(i == 0)
-                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
-            else if(j == 0)
-                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
-            else
-                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
-        }
+    bool solve(int i,int j,int k,string s1,string s2,string s3,vector<vector<int>>& dp){
+        if(i <= 0 && j <= 0 && k <= 0) return true;
         
-    return table[s1.length()][s2.length()];
-}
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(i > 0 && s1[i-1] == s3[k-1] && j > 0 && s2[j-1] == s3[k-1])
+            return dp[i][j] = (solve(i-1,j,k-1,s1,s2,s3,dp) || solve(i,j-1,k-1,s1,s2,s3,dp));
+        else if(i > 0 && s1[i-1] == s3[k-1]) 
+            return dp[i][j] = solve(i-1,j,k-1,s1,s2,s3,dp);
+        else if(j > 0 && s2[j-1] == s3[k-1])
+            return dp[i][j] = solve(i,j-1,k-1,s1,s2,s3,dp);
+        else
+            return dp[i][j] = false;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.size(),m = s2.size();
+        if((n+m) != s3.length()) return false;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,s3.length(),s1,s2,s3,dp);
+    }
 };
