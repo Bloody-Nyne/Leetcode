b39d48a6be71da81035e10a9fa9ca0ee830b59b7
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-09-02 17:16:01 +0530

    Time: 147 ms (66.43%) | Memory: 63.1 MB (84.93%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -3,8 +3,23 @@ public:
     int minExtraChar(string s, vector<string>& dictionary) {
         int n = s.size();
         set<string> st(dictionary.begin(),dictionary.end());
-        vector<int> dp(n,-1);
-        return solve(0,s,st,dp);
+        vector<int> dp(n+1,0);
+        //return solve(0,s,st,dp);
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            string temp = "";
+            int cnt = s.size();
+            for(int j=i;j<s.size();j++){
+                temp += s[j];
+                int cur = st.count(temp) ? 0 : temp.size();
+                int next = dp[j+1];
+                int total = cur + next;
+
+                cnt = min(cnt,total);
+            }
+            dp[i] = cnt;
+        }
+        return dp[0];
     }
     int solve(int i,string s,set<string>& st,vector<int>& dp){
         if(i == s.size()) return 0;
