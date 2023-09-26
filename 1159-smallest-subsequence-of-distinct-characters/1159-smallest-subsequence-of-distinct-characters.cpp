1c1787d080f4fe73cd9594219d62b0fe6756c22e
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-09-27 00:02:43 +0530

    Time: 3 ms (48.89%), Space: 6.9 MB (5.19%) - LeetHub

---
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestSubsequence(string text) {
+        stack<char> st;
+        vector<int> last(256, 0), seen(256, 0);
+        for(int i = 0; i < text.size(); i++)
+            last[text[i]] = i;
+        for(int i = 0; i < text.size(); i++){
+            if(seen[text[i]])
+                continue;
+
+            while(st.size() and st.top() > text[i] and last[st.top()] > i)
+                seen[st.top()] = 0, st.pop(); 
+
+            st.push(text[i]), seen[text[i]] = 1;
+        }
+        string res = "";
+        while(st.size())
+            res += st.top(), st.pop();
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
