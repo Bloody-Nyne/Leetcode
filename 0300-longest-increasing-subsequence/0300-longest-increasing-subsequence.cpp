a148cf74983fc792d915d883106d615791656259
Harshvardhan singh <102972384+Fenrir-04@users.noreply.github.com>
2023-09-30 20:43:38 +0530

    Time: 842 ms (18.6%), Space: 292.5 MB (5.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,33 +2,20 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        //return slave(0,-1,nums,dp);
-        for(int ind=n-1;ind>=0;ind--){
-            for(int p_ind=ind-1;p_ind>=-1;p_ind--){
-                int notTake = 0 + dp[ind+1][p_ind+1];
-                int take = 0;
-                if(p_ind == -1 || nums[ind] > nums[p_ind]){
-                    take = 1 + dp[ind+1][ind+1];
-                }
-                dp[ind][p_ind+1] = max(take,notTake);
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return solve(0,-1,nums,n,dp);
     }
 private:
-    int slave(int ind,int p_ind,vector<int>& nums,vector<vector<int>>& dp){
-        if(ind == nums.size())
-            return 0;
+    int solve(int ind,int p_ind,vector<int>& nums,int n,vector<vector<int>>& dp){
+        if(ind == n) return 0;
 
-        if(dp[ind][p_ind+1] != -1) 
-            return dp[ind][p_ind+1];
+        if(dp[ind][p_ind+1] != -1) return dp[ind][p_ind+1];
 
-        int notTake = 0 + slave(ind+1,p_ind,nums,dp);
-        int take = 0;
+        int len = 0;
+        len = 0 + solve(ind+1,p_ind,nums,n,dp);
         if(p_ind == -1 || nums[ind] > nums[p_ind]){
-            take = 1 + slave(ind+1,ind,nums,dp);
+            len = max(len, 1 + solve(ind+1,ind,nums,n,dp));
         }
-        return dp[ind][p_ind+1] = max(notTake,take);
+        return dp[ind][p_ind+1] = len;
     }
 };
